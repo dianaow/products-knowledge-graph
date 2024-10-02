c5dfd54f64bb27ba198941f0873a633a19173a22
dianaow <diana.ow@outlook.com>
2024-10-02 22:27:48 +0800

    fix scraping

--- web_scrape.py
+++ web_scrape.py
@@ -1,6 +1,6 @@
 # Imports specifics using Firefox web driver.
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 # Imports for general selenium functionality.
 from selenium.webdriver.support.ui import WebDriverWait
@@ -40,9 +40,39 @@ def get_data(file_path):
     This function carries out the 'product urls extraction' pipeline, which is to extract url strings from a locally saved HTML page,
     then enters each url with headless selenium webdriver and extracts the HTML content within each url
     """
+    # Creates an option for how the browser behaves. 
+    opts = Options()
+    #opts.add_argument("--headless")
+
+    # Creates firefox webdriver.
+    driver = Chrome(options=opts)
+
+    # Navigate to the Shopee login page
+    driver.get('https://shopee.sg/buyer/login')
+
+    # Wait for the page to load and find the input fields
+    wait = WebDriverWait(driver, 10)
+    username_field = wait.until(EC.presence_of_element_located((By.NAME, 'loginKey')))
+    password_field = wait.until(EC.presence_of_element_located((By.NAME, 'password')))
+
+    # Enter your username and password
+    username_field.send_keys('(+65) 9845 9301')
+    password_field.send_keys('CocoGummy1984!')
+
+    # Find and click the login button
+    login_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'DYKctS')))
+    login_button.click()
+
+    # Wait for a while for the login process to complete
+    time.sleep(30)
+
     urls = extract_urls(file_path)
+    time.sleep(5)
+    
     for url in urls:
-        save_product_pages(url)
+        save_product_pages(driver, url)
+
+    driver.quit()
 
 def extract_urls(file_path):
     
@@ -58,33 +88,24 @@ def extract_urls(file_path):
 
     return urls
 
-def save_product_pages(url):
+def save_product_pages(driver, url):
     """
     This function extracts the HTML content of a webpage with selenium webdriver
     """  
-    # Creates an option for how the browser behaves. 
-    opts = Options()
-    opts.add_argument("--headless")
-
-    # Creates firefox webdriver.
-    driver = Firefox(options=opts)
-
     # Navigate to the link provided.
     driver.get(url)
 
-    time.sleep(1)
+    time.sleep(10)
     
-    webpage_save_name = "productpages/" + clean(url).replace("https://shopee.sg", "").replace(" ", "_") + ".html"
+    webpage_save_name = "productpages/" + clean(re.sub(r'\d+', '', url)).replace("https://shopee.sg", "").replace(" ", "_") + ".html"
     save_webpage(driver, webpage_save_name)
     print("Web page saved as {}".format(webpage_save_name))
-
-    driver.quit()
 
 def read_webpage_contents(file_path):
     """
     This function reads the HTML content of a webpage from a file
     """  
-    with open(file_path, "r", encoding = "utf-8") as f:
+    with open(file_path, "r", encoding='utf-8', errors='ignore') as f:
         contents = f.read()
         return contents
     
@@ -93,7 +114,7 @@ def save_webpage(driver, file_name):
     This function saves the HTML content of a webpage into a file
     """  
     html_contents = driver.page_source
-    with open(file_name, "w", encoding = "utf8") as f:
+    with open(file_name, "w", encoding='utf-8', errors='ignore') as f:
         f.write(html_contents)
 
 def get_kg():
@@ -120,12 +141,16 @@ def get_kg():
         #except:
             #print('Error in extracting product details from  ' + path)
             #continue
-            
-    # Save the Python object as a JSON file
-    with open('kg.json', 'w') as json_file:
-        json.dump(nodesAll, json_file)
 
-    return nodesAll
+    unique_nodes = set(frozenset(d.items()) for d in nodesAll)
+    unique_nodes = [dict(fs) for fs in unique_nodes]
+    unique_nodes = sorted(unique_nodes, key=lambda d: d['product_id'])
+
+    # Save the Python object as a JSON file
+    with open('graph.json', 'w') as json_file:
+        json.dump(unique_nodes, json_file)
+
+    return unique_nodes
     
 def extract_product_details(file_path, current_id):
     """
@@ -143,21 +168,39 @@ def extract_product_details(file_path, current_id):
     cleaned_product_title = clean(product_title)
 
     #description
-    if item_info.find_all('p', class_='QN2lPu'):
-        product_highlights = [clean(li.text) for li in item_info.find_all('p', class_='QN2lPu') if (li.text != '' or li.text != ' ' or li.text != '\n')]
-        #print(product_highlights)
+    if item_info.find_all('p', class_='QN2lPu') or item_info.find_all('div', class_='QN2lPu'):
+        product_highlights_p = [clean(li.text) for li in item_info.find_all('p', class_='QN2lPu') if (li.text != '' or li.text != ' ' or li.text != '\n')]
+        product_highlights_div = [clean(li.text) for li in item_info.find_all('p', class_='QN2lPu') if (li.text != '' or li.text != ' ' or li.text != '\n')]
+        product_highlights = product_highlights_p +  product_highlights_div
         cleaned_product_highlights = []
         for index, p in enumerate(product_highlights):
             p0 = product_highlights[index-1] if(index > 0) else ""
-            if('key ingredients' == p0.lower()):
-                cleaned_product_highlights.append(p)
-            elif('how to use' in p.lower() or 'ingredients' in p.lower() or 'expiration date' in p.lower() or 'expiry' in p.lower() or 'months' in p.lower() or 'shipped' in p.lower()):
-                cleaned_product_highlights.append("")
-            elif('how to use' in p0.lower() or 'ingredients' in p0.lower()):
-                cleaned_product_highlights.append("")
-            else:
-                cleaned_product_highlights.append(p)
-        cleaned_product_highlights = [item for item in cleaned_product_highlights if item]
+            if('ingredients' not in p.lower() and  
+               'ingredients' not in p0.lower() and 
+               'benefits' not in p.lower() and
+               'how to use' not in p.lower() and 
+               'volume' not in p.lower() and 
+               'feature' not in p.lower() and 
+               #'how to use' not in p0.lower() and
+               'tips' not in p.lower() and 
+               'tips' not in p0.lower() and
+               'targets' not in p.lower() and
+               'skin type' not in p.lower() and
+               'free sample' not in p.lower() and 
+               cleaned_product_title.lower() not in p.lower() and
+               '' != p.lower()
+            ):
+                cleaned_product_highlights.append(p.lower())
+            # p0 = product_highlights[index-1] if(index > 0) else ""
+            # if('key ingredients' == p0.lower()):
+            #     cleaned_product_highlights.append(p)
+            # elif('how to use' in p.lower() or 'ingredients' in p.lower() or 'expiration date' in p.lower() or 'expiry' in p.lower() or 'months' in p.lower() or 'shipped' in p.lower()):
+            #     cleaned_product_highlights.append("")
+            # elif('how to use' in p0.lower() or 'ingredients' in p0.lower()):
+            #     cleaned_product_highlights.append("")
+            # else:
+            #     cleaned_product_highlights.append(p)
+        #cleaned_product_highlights = [item for item in cleaned_product_highlights if item]
         #print(cleaned_product_highlights)
     else:
         meta_description = item_info.find('meta', attrs={'name': 'description'})
@@ -194,16 +237,33 @@ def extract_product_details(file_path, current_id):
     else:
         volume = 0
 
+    # number of favourites
+    fav_num = [int(re.search(r'\d+', element).group()) for element in item_info(text=re.compile("Favorite")) if re.search(r'\d+', element)][0]
+
+    # number of items sold
+    div = item_info.find('div', class_='AcmPRb')
+    sold_num = div.text if div else None
+
     nodes.append({
         "product_id": current_id,
         "product_url": img_src,
-        "product": category,
+        #"product": category,
+        "product": 'Exfoliators & Scrubs',
         "title": cleaned_product_title,
         "price":  min_price,
         "volume": volume,
-        "description": ", ".join(cleaned_product_highlights)
+        "fav_num": fav_num,
+        "sold_num": sold_num
     })
     
+    for value in cleaned_product_highlights:
+        nodes.append({
+            "product_id": current_id,
+            "relationship": "hasDescription",
+            "entity_type": "description",
+            "entity_value": value, 
+        })
+                 
     for index, key in enumerate(specification_keys):
         value = specification_values[index]        
         if key == "Brand":
@@ -256,13 +316,14 @@ def extract_product_details(file_path, current_id):
     return nodes
 
 
-folder_path = 'productoverviewpages'
-file_names = [folder_path + '/' + f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-file_names.sort()
+# folder_path = 'productoverviewpages'
+# file_names = [folder_path + '/' + f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+# file_names.sort()
 
-nodesAll = []
-for path in file_names:
-    print(path)
-    get_data(path)
+
+# nodesAll = []
+# for path in file_names:
+#     print(path)
+#     get_data(path)
 
 get_kg()
